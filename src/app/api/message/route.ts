9108ea7bf6e0c4e8a44480333e7af92473d29424
princeoncada <prince.oncada@gmail.com>
2026-04-15 22:30:25 +0800

    feat: add endpoint for sending messages

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,40 @@
+import { db } from "@/db";
+import { sendMessageValidator } from "@/lib/validators/SendMessageValidators";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  // endpoint for asking a question for a PDF file
+
+  const body = await req.json()
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const userId = user?.id
+
+  if (!userId)
+    return new Response('Unauthorized', { status: 401 })
+
+  const { fileId, message} = sendMessageValidator.parse(body)
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId,
+    },
+  })
+
+  if (!file) 
+    new Response('Not found', { status: 404 })
+
+  await db.message.create({
+    data: {
+      text: message,
+      isUserMessage: true,
+      userId,
+      fileId
+    }
+  })
+  
+}
