5a9d395e34c81cddcffb91fd889c80deee96979a
princeoncada <prince.oncada@gmail.com>
2026-04-15 22:25:51 +0800

    feat(chat): add textarea input with send button UI

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,7 +1,35 @@
-const ChatInput = () => {
-  return (
-    <div>ChatInput</div>
-  )
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "../ui/button";
+import { Send } from "lucide-react";
+
+interface ChatInputProps {
+  isDisabled?: boolean;
 }
 
-export default ChatInput
+const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  return (
+    <div className="absolute bottom-0 left-0 w-full">
+      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full grow p-4">
+            <div className="relative">
+              <Textarea
+                rows={1}
+                maxRows={4}
+                autoFocus
+                placeholder="Enter your question..."
+                className="resize-none pr-12 text-base py-3 scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-thumb-blue scrollbar-w-2 scrolling-touch"
+              />
+
+              <Button size="icon-lg" aria-label="send message" className="absolute bottom-1.25 right-2">
+                <Send className="h-4 w-4"/>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ChatInput;
