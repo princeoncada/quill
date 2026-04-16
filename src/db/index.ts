91fb8ae19538252c5f92742827b95f367f5e7bf7
princeoncada <prince.oncada@gmail.com>
2026-04-16 16:57:04 +0800

    fix(db): assert DATABASE_URL to ensure PrismaPg adapter receives valid connection string

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "../generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 
 declare global {
   var cachedPrisma: PrismaClient
