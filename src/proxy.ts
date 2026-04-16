cdfb60bf6aa1c5d2d60ef0b0ea808437a7fecdde
princeoncada <prince.oncada@gmail.com>
2026-04-17 02:14:35 +0800

    feat(auth): add Kinde middleware with public path protection rules

---
+++ src/proxy.ts
@@ -0,0 +1,19 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default withAuth(
+  async function proxy() { },
+  {
+    publicPaths: [
+      "/",
+      "/pricing",
+      "/api/auth/(.*)",
+      "/api/webhooks/stripe",
+    ],
+  }
+);
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  ],
+};
