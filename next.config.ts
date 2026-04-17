274495eb717e2b05da14c954b772d937f4da5934
princeoncada <prince.oncada@gmail.com>
2026-04-17 19:22:17 +0800

    feat(next): add redirects for sign-in and sign-up routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/sign-in",
+        destination: "/api/auth/login",
+        permanent: true
+      },
+      {
+        source: "/sign-up",
+        destination: "/api/auth/register",
+        permanent: true
+      },
+    ]
+  }
 };
 
 export default nextConfig;
