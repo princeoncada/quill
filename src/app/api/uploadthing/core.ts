5a5ba56cbceba6562a0b86fbdec92f52eed18461
princeoncada <prince.oncada@gmail.com>
2026-04-17 19:24:22 +0800

    feat(upload): add plan-based upload routes with subscription limits and duplicate file checks

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -5,6 +5,8 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { pinecone } from "@/lib/pinecone";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
 
 const f = createUploadthing();
 
@@ -48,71 +50,124 @@ async function indexPdfDocuments(args: {
   await pineconeIndex.namespace(namespace).upsert({ records })
 }
 
+const middleware = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser()
+  if (!user || !user.id) throw new TRPCError({ code: "UNAUTHORIZED" })
+
+  const subscriptionPlan = await getUserSubscriptionPlan()
+
+  return { subscriptionPlan, userId: user.id }
+}
+
+const onUploadComplete = async ({
+  metadata,
+  file
+}: {
+  metadata: Awaited<ReturnType<typeof middleware>>
+  file: {
+    key: string,
+    name: string,
+    ufsUrl: string
+  }
+}) => {
+
+  const isFileExist = await db.file.findFirst({
+    where: {
+      key: file.key
+    }
+  })
+
+  if (isFileExist) return;
+
+  const createdFile = await db.file.create({
+    data: {
+      key: file.key,
+      name: file.name,
+      userId: metadata.userId,
+      url: file.ufsUrl,
+      uploadStatus: "PROCESSING"
+    }
+  })
+
+  try {
+    const response = await fetch(file.ufsUrl)
+
+    console.log("2. Response status:", response.status, response.statusText)
+    if (!response.ok) {
+      throw new Error(`Failed to fetch uploaded file: ${response.status} ${response.statusText}`)
+    }
+
+    const blob = await response.blob()
+
+    const loader = new PDFLoader(blob)
+
+    const pageLevelDocs = await loader.load()
+
+    const pagesAmt = pageLevelDocs.length
+
+    const { subscriptionPlan } = metadata
+    const { isSubscribed } = subscriptionPlan
+
+    const isProExceeded = pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf
+    const isFreeExceeded = pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf
+
+
+    if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
+      await db.file.update({
+        data: {
+          uploadStatus: "FAILED"
+        },
+        where: {
+          id: createdFile.id
+        }
+      })
+    }
+
+    await indexPdfDocuments({
+      docs: pageLevelDocs,
+      namespace: createdFile.id
+    })
+
+    await db.file.update({
+      data: {
+        uploadStatus: "SUCCESS",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    })
+
+  } catch (err) {
+    await db.file.update({
+      data: {
+        uploadStatus: "FAILED",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    })
+    console.error(err)
+  }
+}
+
 export const ourFileRouter = {
-  pdfUploader: f({
+  freePlanUploader: f({
     pdf: {
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
   })
-    .middleware(async () => {
-      const { getUser } = getKindeServerSession();
-      const user = await getUser()
-      if (!user || !user.id) throw new TRPCError({ code: "UNAUTHORIZED" })
-
-      return { userId: user.id }
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      const createdFile = await db.file.create({
-        data: {
-          key: file.key,
-          name: file.name,
-          userId: metadata.userId,
-          url: file.ufsUrl,
-          uploadStatus: "PROCESSING"
-        }
-      })
-
-      try {
-        const response = await fetch(file.ufsUrl)
-
-        console.log("2. Response status:", response.status, response.statusText)
-        if (!response.ok) {
-          throw new Error(`Failed to fetch uploaded file: ${response.status} ${response.statusText}`)
-        }
-
-        const blob = await response.blob()
-
-        const loader = new PDFLoader(blob)
-
-        const pageLevelDocs = await loader.load()
-
-        await indexPdfDocuments({
-          docs: pageLevelDocs,
-          namespace: createdFile.id
-        })
-
-        await db.file.update({
-          data: {
-            uploadStatus: "SUCCESS",
-          },
-          where: {
-            id: createdFile.id,
-          },
-        })
-
-      } catch (err) {
-        await db.file.update({
-          data: {
-            uploadStatus: "FAILED",
-          },
-          where: {
-            id: createdFile.id,
-          },
-        })
-        console.error(err)
-      }
-    }),
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
+  proPlanUploader: f({
+    pdf: {
+      maxFileSize: "16MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
