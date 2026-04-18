994f4ac61e53b1144d2951e0a077e45a8fe1288a
princeoncada <prince.oncada@gmail.com>
2026-04-18 14:48:58 +0800

    refactor(billing): simplify plan display and move renewal status into card description

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -35,10 +35,17 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
         <Card>
           <CardHeader>
             <CardTitle>Subscription Plan</CardTitle>
-            <CardDescription>You are currently on the <strong>{subscriptionPlan.name}</strong> plan</CardDescription>
+            <CardDescription>You are currently on the <strong>{subscriptionPlan.isSubscribed ? "Pro" : "Free"}</strong> plan {" | "}
+              {subscriptionPlan.isSubscribed ? (
+                <span className="font-medium">
+                  {subscriptionPlan.isCanceled ? "To be canceled on " : "Renews on "}
+                  {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'MM/dd/yyyy')}
+                </span>
+              ) : null}
+            </CardDescription>
           </CardHeader>
-          <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
-            <Button type="submit" size="lg" className="bg-blue-600 hover:bg-blue-700">
+          <CardFooter className="flex flex-col items-start space-y-2">
+            <Button type="submit" size="lg" className="bg-blue-600 hover:bg-blue-700 hover:cursor-pointer">
               {
                 isPending ? (
                   <Loader2 className="mr-4 h-4 w-4 animate-spin" />
@@ -49,12 +56,6 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
               }
             </Button>
 
-            {subscriptionPlan.isSubscribed ? (
-              <p className="rounded-full text-xs font-medium">
-                {subscriptionPlan.isCanceled ? "Your plan will be canceled on " : "Your plan renews on"}
-                {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.MM.yyyy')}
-              </p>
-            ) : null}
           </CardFooter>
         </Card>
       </form>
